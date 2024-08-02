b00402b470b3c0e28f7d15482862587ae950cee6
Raja <raja28april@gmail.com>
2024-08-02 17:14:42 +0200

    p, puts and print methods diff

---
+++ 02 - Getting Started/the_p_method.rb
@@ -0,0 +1,5 @@
+p "P method adds"
+p " a line break"
+p "but it also\n includes escape sequence and quotes in the output"
+p "p method is used for developer debugging and "
+puts "puts method is used for end user view"
