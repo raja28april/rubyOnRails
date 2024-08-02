49222e9a04b4f3654f44687373607c5e6c32e360
Raja <raja28april@gmail.com>
2024-08-02 17:28:43 +0200

    arithmetic

---
+++ 02 - Getting Started/basic_arithmetic_in_ruby.rb
@@ -0,0 +1,13 @@
+# PEMDAS
+# Paranthesis , Exponents, Multi, Div, Add, Sub
+puts (2 * 3) + 6
+puts 10 / 5 # 2
+puts 12 / 5 # 2
+
+puts 12.0 / 5 # 2.4
+puts 12 / 5.0 # 2.4
+puts 0.5 / 2 # 0.25
+
+puts 5 % 2 # 1
+puts 6 % 2 # 1
+puts 0.5 % 2 # 1
