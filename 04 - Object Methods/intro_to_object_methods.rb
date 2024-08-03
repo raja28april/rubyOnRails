239dc160fa8393340e2271ac338692efeb35595c
Raja <raja28april@gmail.com>
2024-08-03 23:42:03 +0200

    introToObjMethods

---
+++ 04 - Object Methods/intro_to_object_methods.rb
@@ -0,0 +1,13 @@
+puts "Hello".length # invoking length method on the string object "Hello"
+puts "Hello".upcase # invoking upcase method on the string object "Hello"
+puts "HeLlo".downcase # invoking downcase method on the string object "Hello"
+
+expression = "TOTALLY Different"
+
+puts expression.downcase
+puts expression
+
+=begin 
+Note: In Ruby, we can optionally provide () when invoking a method, 
+i.e, "Hello".length can also written as "Hello".length()
+=end
