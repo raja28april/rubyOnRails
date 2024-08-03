2e90325a3b562b52229398e9edb4dac7f36bb782
Raja <raja28april@gmail.com>
2024-08-03 23:58:56 +0200

    updatedCommentForIntegerMethod

--- 04 - Object Methods/integer_methods.rb
+++ 04 - Object Methods/integer_methods.rb
@@ -4,6 +4,6 @@ puts -1.next
 puts 10.succ
 puts 10.pred
 
-val = puts 20.next # incorrect
-val += 1 # due to previous step --> undefined method `+' for nil (NoMethodError)
+val = puts 20.next # incorrect because puts method returns nil
+# val += 1 # due to previous step --> undefined method `+' for nil (NoMethodError)
 puts val
