cc96aa1581c4a179522f835f8121ba9fc5960af7
Raja <raja28april@gmail.com>
2024-08-03 23:37:02 +0200

    const

---
+++ 03 - Variables/constants.rb
@@ -0,0 +1,9 @@
+TAX_RAte = 10
+TAX_RAte += 10
+# TAX_RATE =20
+# TAX_RATE +=20
+
+# puts TAX_RATE
+puts TAX_RAte
+
+# NOTE: even though it is case sensitive it is not recommended to use same variable with different case types(LOWERCASE or UPPERCASE)
