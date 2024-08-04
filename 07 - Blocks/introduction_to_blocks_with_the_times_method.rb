d78773cb7fab0d88be137c8af81b4ae3cdd03e80
Raja <raja28april@gmail.com>
2024-08-04 19:33:06 +0200

    block - times method

---
+++ 07 - Blocks/introduction_to_blocks_with_the_times_method.rb
@@ -0,0 +1,16 @@
+value = 3.times do
+  puts "Ruby is simble"
+  puts "Ruby valara simble aanu"
+  puts "Ruby is powerfulllllll"
+end
+
+def print_five_times
+  5.times {print "Hello"}
+end
+
+print_five_times()
+
+def money_printer(num)
+  num.times {print "Money"}
+end
+money_printer(3)
