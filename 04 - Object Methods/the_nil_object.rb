8d6dbc906e4e3c548c5e9432228c6fb07ad1b652
Raja <raja28april@gmail.com>
2024-08-04 07:20:09 +0200

    nil object explanation

--- 04 - Object Methods/the_nil_object.rb
+++ 04 - Object Methods/the_nil_object.rb
@@ -1,4 +1,9 @@
-puts nil # prints nothing
+puts nil 
+=begin 
+Above line prints nothing because,
+puts method prints the string representation, 
+and there is no string representation for nil
+=end
 p nil # prints nil
 
 val = puts "hi" # saves nil as puts method retuns nil
