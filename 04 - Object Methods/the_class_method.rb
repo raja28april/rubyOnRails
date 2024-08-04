18c18f2bb037e5e72af1210e71a849a82510d960
Raja <raja28april@gmail.com>
2024-08-04 09:41:07 +0200

    class method

---
+++ 04 - Object Methods/the_class_method.rb
@@ -0,0 +1,11 @@
+# class - blue print of an object (template/schematic)
+# Object - an instance of the class
+puts "hello".class # String
+puts "123$".class # String
+
+puts 1.class # Integer
+puts -102.class # Integer
+puts 999_999_999.class # Integer
+
+puts 1.5.class # Float
+puts 3.14.class # Float
