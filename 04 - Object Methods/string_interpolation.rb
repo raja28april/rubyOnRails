bba937ed714f29b530c8cc1503efff219f0971bd
Raja <raja28april@gmail.com>
2024-08-04 07:21:20 +0200

    stringInterpolation

---
+++ 04 - Object Methods/string_interpolation.rb
@@ -0,0 +1,10 @@
+name = "Raja"
+puts "Hello #{name}, How are you?"
+
+age = 23
+puts "I am #{age} years young?"
+
+puts "In 5 years I will be #{age + 5} "
+
+x, y = 5, 6
+puts "The sum of #{x} and #{y} is #{x + y}"
