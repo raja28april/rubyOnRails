99915dfa96d21f8fcb4b4f69c45cc95b04383855
Raja <raja28april@gmail.com>
2024-08-04 11:06:18 +0200

    FloatMethods

---
+++ 05 - Booleans and Predicate Methods/float_methods.rb
@@ -0,0 +1,17 @@
+puts 10.5.floor # floor rounds down --> output: 10
+puts 10.9.floor # floor rounds down --> output: 10
+
+puts 10.9.ceil # ceil rounds up --> output: 11
+puts 10.2.ceil # ceil rounds up --> output: 11
+puts 10.2.ceil.class # Integer
+
+puts
+
+puts 10.2.round # 10
+puts 10.9.round # 11
+puts 10.5.round # 11
+puts 3.1459.round(2) # 3.14
+
+puts 5.35.abs # distance of a value from 0 --> output: 5.35
+puts (-5.35).abs # distance of a value from 0 --> output: 5.35
+
